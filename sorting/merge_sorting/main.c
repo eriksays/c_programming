d5dea6ac69bdaa7c6a7f0d0a24b05968d4ecdf92
matthew collins <matthew.collins@geekluv.com>
2022-07-17 14:08:50 -0400

    update

--- sorting/merge_sorting/main.c
+++ sorting/merge_sorting/main.c
@@ -1,62 +1,94 @@
 #include <stdio.h>
 
-
 void merge_sort(int a[], int length);
 void merge_sort_recursion(int a[], int l, int r);
 void merge_sorted_arrays(int a[], int l, int m, int r);
 
-
 int main()
 {
-
-    int a[] = { 7,1,3,9,0,2,4,5,8,6};
-    int length = 10;
-    
-    merge_sort(a, length);
-
-    for ( int i = 0; i < length; i++ ) {
-        printf("a[%d] = %d\n", i, a[i]);
-    }
-
-    return 0;
+  int array[] = { 9, 4, 8, 1, 7, 0, 3, 2, 5, 6};
+  int length = 10;
+  
+  // sort the array using merge sort
+  merge_sort(array, length);
+  
+  // print out the array elements to verify they have been sorted
+  for (int i = 0; i < length; i++)
+    printf("%d ", array[i]);
+  printf("\n");
+  
+  return 0;
 }
 
 void merge_sort(int a[], int length)
 {
-    merge_sort_recursion(a, 0, length - 1);
+  merge_sort_recursion(a, 0, length - 1);
 }
 
 void merge_sort_recursion(int a[], int l, int r)
 {
-    if (l < r)
-    {
-        int m = l + (r-1) / 2; //get middle index of array
-        merge_sort_recursion(a, l, m);
-        merge_sort_recursion(a, m+1, r);
-
-        merge_sorted_arrays(a, l, m, r);
-    }
-    
+  if (l < r)
+  {
+    int m = l + (r - l) / 2;
+  
+    merge_sort_recursion(a, l, m);
+    merge_sort_recursion(a, m + 1, r);
+  
+    merge_sorted_arrays(a, l, m, r);
+  }
 }
 
 void merge_sorted_arrays(int a[], int l, int m, int r)
 {
-    int left_length = m - l + 1;
-    int right_length = r - m;
-
-    int temp_left[left_length];
-    int temp_right[right_length];
-    int i, j, k;
-
-    for (int i = 0; i < left_length; i++)
-        temp_left[i] = a[l+i];
-
-    for (int i =0; i < right_length; i++)
-        temp_right[i] = a[m + 1 + i];
-
-    for (i = 0, j = 0, k = l; k <= r; k++)
+  int left_length = m - l + 1;
+  int right_length = r - m;
+  
+  int temp_left[left_length];
+  int temp_right[right_length];
+  
+  int i, j, k;
+  
+  for (int i = 0; i < left_length; i++)
+    temp_left[i] = a[l + i];
+  
+  for (int i = 0; i < right_length; i++)
+    temp_right[i] = a[m + 1 + i];
+  
+  for (i = 0, j = 0, k = l; k <= r; k++)
+  {
+    if ((i < left_length) &&
+        (j >= right_length || temp_left[i] <= temp_right[j]))
     {
-        
+      a[k] = temp_left[i];
+      i++;
     }
+    else
+    {
+      a[k] = temp_right[j];
+      j++;
+    }
+  }  
 }
 
+
+// Example visualization of the merge sort algorithm:
+//
+//          [38, 27, 43, 3, 9, 82, 10]
+//                     /   \
+//       [38, 27, 43, 3]   [9, 82, 10]
+//        /         |         |      \
+//   [38, 27]    [43, 3]   [9, 82]   [10]
+//    /   |      /    |    /    \      |
+// [38]  [27]  [43]  [3]  [9]   [82]  [10]
+//    \  /       \   /     \     /     |
+//   [27, 38]    [3, 43]   [9, 82]    [10]
+//       \         /          \        /
+//     [3, 27, 38, 43]        [9, 10, 82]
+//           \                  /
+//          [3, 9, 10, 27, 38, 43, 82]
+//
+// The array is first broken up into progressively smaller unsorted portions of
+// the array, and once we have "sub-arrays" of 1 element they are by definition
+// sorted arrays.  From here the "sorted arrays" are merged together until we 
+// arrive at the complete sorted array.
+//
